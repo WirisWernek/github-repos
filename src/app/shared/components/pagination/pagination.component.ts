bbd72d9ab5b577fd3c61c7f3a79caeed27a3ecf7
Wiris Wernek <rafaeljwerneck43@gmail.com>
2024-03-03 21:54:45 -0300

    fix: atualiza para angular 17

--- src/app/shared/components/pagination/pagination.component.ts
+++ src/app/shared/components/pagination/pagination.component.ts
@@ -2,59 +2,59 @@ import { AfterViewInit, Component, EventEmitter, Input, Output } from '@angular/
 import { PaginatorModel } from 'src/app/models/Paginator.model';
 
 @Component({
-   selector: 'app-pagination',
-   templateUrl: './pagination.component.html',
-   styleUrls: ['./pagination.component.scss'],
+    selector: 'app-pagination',
+    templateUrl: './pagination.component.html',
+    styleUrls: ['./pagination.component.scss'],
 })
 export class PaginationComponent implements AfterViewInit {
-   @Output() emmiter = new EventEmitter<PaginatorModel>();
-   @Input() reposLength!: number;
+    @Output() emmiter = new EventEmitter<PaginatorModel>();
+    @Input() reposLength!: number;
 
-   paginator: PaginatorModel;
-   totalPage: number = 1;
+    paginator: PaginatorModel;
+    totalPage: number = 1;
 
-   de: number;
-   ate: number;
+    de: number;
+    ate: number;
 
-   constructor() {
-      this.paginator = new PaginatorModel();
-      this.de = this.paginator.page;
-      this.ate = this.paginator.perPage;
-   }
+    constructor() {
+        this.paginator = new PaginatorModel();
+        this.de = this.paginator.page;
+        this.ate = this.paginator.perPage;
+    }
 
-   ngAfterViewInit(): void {
-      this.calculatePages();
-   }
+    ngAfterViewInit(): void {
+        this.calculatePages();
+    }
 
-   calculatePages() {
-      this.totalPage = Math.ceil(this.reposLength / this.paginator.perPage);
-      this.calcularItens();
-   }
+    calculatePages() {
+        this.totalPage = Math.ceil(this.reposLength / this.paginator.perPage);
+        this.calcularItens();
+    }
 
-   next() {
-      this.paginator.page = this.paginator.page + 1;
-      this.emmiter.emit(this.paginator);
-      this.calculatePages();
-   }
+    next() {
+        this.paginator.page = this.paginator.page + 1;
+        this.emmiter.emit(this.paginator);
+        this.calculatePages();
+    }
 
-   prev() {
-      this.paginator.page = this.paginator.page - 1;
-      this.emmiter.emit(this.paginator);
-      this.calculatePages();
-   }
+    prev() {
+        this.paginator.page = this.paginator.page - 1;
+        this.emmiter.emit(this.paginator);
+        this.calculatePages();
+    }
 
-   calcularItens() {
-      let count = this.paginator.page * this.paginator.perPage - this.paginator.perPage;
-      let delimiter = count + this.paginator.perPage;
+    calcularItens() {
+        let count = this.paginator.page * this.paginator.perPage - this.paginator.perPage;
+        let delimiter = count + this.paginator.perPage;
 
-      if (delimiter <= this.reposLength) {
-         this.de = count + 1;
-         this.ate = delimiter;
-      } else if (this.reposLength == 0) {
-         setTimeout(() => this.calcularItens(), 5000);
-      } else {
-         this.de = count + 1;
-         this.ate = this.reposLength;
-      }
-   }
+        if (delimiter <= this.reposLength) {
+            this.de = count + 1;
+            this.ate = delimiter;
+        } else if (this.reposLength == 0) {
+            setTimeout(() => this.calcularItens(), 5000);
+        } else {
+            this.de = count + 1;
+            this.ate = this.reposLength;
+        }
+    }
 }
